f0a4504120de720674e2c4197c8441ea343cc042
niconan <nick.hax@gmail.com>
2015-09-28 23:19:39 -0700

    setup conditional navbar, add bootstrap theming

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ source "https://rails-assets.org" do
   gem "rails-assets-angular-devise"
 end
 
+# bootstrap
+gem "twitter-bootstrap-rails"
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929003116) do
+ActiveRecord::Schema.define(version: 20150929051807) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
